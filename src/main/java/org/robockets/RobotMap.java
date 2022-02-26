1799a09c27cc8eaa056b09c8ef669a07b20eb8b6
Gian Sabia <43626786+GianSabia@users.noreply.github.com>
2022-02-26 14:09:01 -0500

    I Messed up the IDs on the intake Motors.

--- src/main/java/org/robockets/RobotMap.java
+++ src/main/java/org/robockets/RobotMap.java
@@ -45,8 +45,8 @@ public class RobotMap {
     public static DifferentialDriveOdometry m_odometry = new DifferentialDriveOdometry(gyro.getRotation2d());
 
     // Intake
-    public static CANSparkMax back_intake = new CANSparkMax(3, CANSparkMaxLowLevel.MotorType.kBrushed);
-    public static CANSparkMax front_intake = new CANSparkMax(4, CANSparkMaxLowLevel.MotorType.kBrushed);
+    public static CANSparkMax back_intake = new CANSparkMax(9, CANSparkMaxLowLevel.MotorType.kBrushed);
+    public static CANSparkMax front_intake = new CANSparkMax(10, CANSparkMaxLowLevel.MotorType.kBrushed);
     // Is this DIO ports?
     public static Solenoid back_pull = new Solenoid(PneumaticsModuleType.CTREPCM,0);
     public static Solenoid front_pull = new Solenoid(PneumaticsModuleType.CTREPCM,1);
