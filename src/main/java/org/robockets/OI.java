ab0ee63ffae2575797b70dcd955603ca23bc6134
Gian Sabia <43626786+GianSabia@users.noreply.github.com>
2022-03-02 20:43:14 -0500

    More additions to swapping out which layout we're using

--- src/main/java/org/robockets/OI.java
+++ src/main/java/org/robockets/OI.java
@@ -112,32 +112,32 @@ public class OI {
      * Yes I KNOW this is a repeat, but
      * Allister is just THAT impressive with how he was able to set everything up that
      * I just HAVE to use it!
-     *
+     * <p>
      * The order of the buttons are starting at the top left of the button board and working down and to the right
      * The order is assuming that the green button is at the bottom
      */
-    public static final JoystickButton B0 = new JoystickButton(manual,9);
-    public static final JoystickButton B1 = new JoystickButton(manual, 8);
-    public static final JoystickButton B2 = new JoystickButton(manual, 12);
-    public static final JoystickButton B3 = new JoystickButton(manual, 3);
-    public static final JoystickButton B4 = new JoystickButton(manual, 13);
-    public static final JoystickButton B5 = new JoystickButton(manual, 7);
-    public static final JoystickButton B6 = new JoystickButton(auto, 2);
-    public static final JoystickButton B7 = new JoystickButton(manual, 11);
-    public static final JoystickButton S = new JoystickButton(manual, 5);
-    public static final JoystickButton B8 = new JoystickButton(auto, 10);
-    public static final JoystickButton B9 = new JoystickButton(auto, 23);
-    public static final JoystickButton B10 = new JoystickButton(auto, 3);
-    public static final JoystickButton B11 = new JoystickButton(auto, 8);
-    public static final JoystickButton B12 = new JoystickButton(auto, 4);
-    public static final JoystickButton B13 = new JoystickButton(auto, 5);
-    public static final JoystickButton B14 = new JoystickButton(auto, 12);
-    public static final JoystickButton B15 = new JoystickButton(auto, 20);
-    public static final JoystickButton B16 = new JoystickButton(auto, 13);
-    public static final JoystickButton B17 = new JoystickButton(auto, 21);
-    public static final JoystickButton B18 = new JoystickButton(auto, 7);
-    public static final JoystickButton B19 = new JoystickButton(auto, 19);
-    public static JoystickButton EMERGENCYSTOP = B9; // Never used, the space bar is too fire
+    public static JoystickButton B0;
+    public static JoystickButton B1;
+    public static JoystickButton B2;
+    public static JoystickButton B3;
+    public static JoystickButton B4;
+    public static JoystickButton B5;
+    public static JoystickButton B6;
+    public static JoystickButton B7;
+    public static JoystickButton S;
+    public static JoystickButton B8;
+    public static JoystickButton B9;
+    public static JoystickButton B10;
+    public static JoystickButton B11;
+    public static JoystickButton B12;
+    public static JoystickButton B13;
+    public static JoystickButton B14;
+    public static JoystickButton B15;
+    public static JoystickButton B16;
+    public static JoystickButton B17;
+    public static JoystickButton B18;
+    public static JoystickButton B19;
+    //public static JoystickButton EMERGENCYSTOP = B9; // Never used, the space bar is also fire
 
     /**
      * These are the main buttons that any controller setup SHOULD handle
@@ -174,6 +174,7 @@ public class OI {
                 activate_inner_wheel = dc_y;
                 break;
             case TwoXbox:
+                secondaryController = new Joystick(1);
                 hood_raise = new JoystickButton(secondaryController, 5); //left trigger button
                 hood_lower = new JoystickButton(secondaryController, 6); //right trigger button
                 intake_pneumatic = sc_b;
@@ -181,11 +182,13 @@ public class OI {
                 activate_fly_wheel_max_speed = sc_y;
                 break;
             case XboxAndButtonBoard:
+                constructButtonBoard();
+                constructButtons(); // THE COOL BOY
                 manual = new Joystick(1);
                 auto = new Joystick(2);
 
-                //public static final JoystickButton thing1 = B0;
-                //public static final JoystickButton thing2 = B1;
+                //thing1 = B0;
+                //thing2 = B1;
                 intake_pneumatic = B2;
                 hood_raise = B3;
                 hood_lower = B4;
@@ -193,7 +196,7 @@ public class OI {
                 activate_fly_wheel_max_speed = B6; // this might also activate the wheel in the shaft
                 activate_inner_wheel = B7;
                 auto1 = B8;
-                EMERGENCYSTOP = B9;
+                //EMERGENCYSTOP = B9;
                 auto2 = B10;
                 break;
             case FlightStick:
@@ -206,9 +209,10 @@ public class OI {
      */
     private ControllerMode deltaSelected;
     public void periodic() {
-        switch(selectedMode) {
+        switch (selectedMode) {
             // Despite the button board being used, certain values just work better with the xbox controller
-            case OneXbox: case XboxAndButtonBoard:
+            case OneXbox:
+            case XboxAndButtonBoard:
                 climberValue = driverController.getRawAxis(5); // up and down on second stick
                 shooterValue = driverController.getRawAxis(4); // right trigger
                 intakeValue = driverController.getRawAxis(3); // left trigger
@@ -222,14 +226,84 @@ public class OI {
                 // no nothing lol
                 break;
             default:
-                DriverStation.reportError("Choose a controller mode!",false);
+                DriverStation.reportError("Choose a controller mode!", false);
         }
 
         // If we've changed the type of drive mode, update everything here
-        if(deltaSelected != selectedMode) {
+        if (deltaSelected != selectedMode) {
+            // Destroy the existing buttons
+            secondaryController = null;
+            destructButtons();
+            destructButtonBoard();
+
+            // Change which controller is being used
             changeDriveMode();
         }
         deltaSelected = selectedMode;
+    }
+
+    /**
+     * Both of these commands are responsible for preventing a NullReferenceException
+     */
+    private void constructButtonBoard() {
+        manual = new Joystick(1);
+        auto = new Joystick(2);
+    }
+    private void destructButtonBoard() {
+        manual = null;
+        auto = null;
+    }
+
+    /**
+     * Both of these commands are responsible for preventing a NullReferenceException
+     */
+    private void constructButtons() {
+        assert(manual != null);
+        assert(auto != null);
+        B0 = new JoystickButton(manual, 9);
+        B1 = new JoystickButton(manual, 8);
+        B2 = new JoystickButton(manual, 12);
+        B3 = new JoystickButton(manual, 3);
+        B4 = new JoystickButton(manual, 13);
+        B5 = new JoystickButton(manual, 7);
+        B6 = new JoystickButton(auto, 2);
+        B7 = new JoystickButton(manual, 11);
+        S = new JoystickButton(manual, 5);
+        B8 = new JoystickButton(auto, 10);
+        B9 = new JoystickButton(auto, 23);
+        B10 = new JoystickButton(auto, 3);
+        B11 = new JoystickButton(auto, 8);
+        B12 = new JoystickButton(auto, 4);
+        B13 = new JoystickButton(auto, 5);
+        B14 = new JoystickButton(auto, 12);
+        B15 = new JoystickButton(auto, 20);
+        B16 = new JoystickButton(auto, 13);
+        B17 = new JoystickButton(auto, 21);
+        B18 = new JoystickButton(auto, 7);
+        B19 = new JoystickButton(auto, 19);
+    }
+    private void destructButtons() {
+        B0 = null;
+        B1 = null;
+        B2 = null;
+        B3 = null;
+        B4 = null;
+        B5 = null;
+        B6 = null;
+        B7 = null;
+        S = null;
+        B8 = null;
+        B9 = null;
+        B10 = null;
+        B11 = null;
+        B12 = null;
+        B13 = null;
+        B14 = null;
+        B15 = null;
+        B16 = null;
+        B17 = null;
+        B18 = null;
+        B19 = null;
     }
 
 }
